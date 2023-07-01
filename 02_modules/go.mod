e56e9658568cca5a1e6f0c1a7478742272258179
kalielze <kalielze@LAPTOP-2IH3HSPM.localdomain>
2023-07-02 00:00:40 +0700

    update learn golang using module 1 July 2023

--- 02_modules/go.mod
+++ 02_modules/go.mod
@@ -1,3 +1,5 @@
 module github.com/dptsi/backend-kalielze
 
 go 1.18
+
+require github.com/kalielze/go-module v1.0.0 // indirect
---
+++ 02_modules/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	go_module "github.com/kalielze/go-module"
+)
+
+func main() {
+	fmt.Println(go_module.SayHello())
+}
